15f1aeefad1e7ec223177da3cbb6ca831a991363
EloiMusk <elioteuber@outlook.com>
2022-06-30 19:25:58 +0200

    split map approach into seperate branch and reset changes

--- src/views/Material/Material.ts
+++ src/views/Material/Material.ts
@@ -1,31 +1,11 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import StorageLoaction from "@/models/StorageLoaction";
-import {Map, View} from "ol";
-import TileLayer from "ol/layer/Tile";
-import {OSM} from "ol/source";
 
 @Component({
     name: 'Material'
 })
 export default class Material extends Vue {
-
-    mounted(): void {
-        this.storageLoactions.forEach(storageLoaction => {
-            const el = this.$refs[storageLoaction.id] as Array<HTMLElement>;
-            const map = new Map({
-                layers: [
-                    new TileLayer({source: new OSM()})
-                ],
-                view: new View({
-                    center: [storageLoaction.x, storageLoaction.y],
-                    zoom: 17,
-                    projection: 'EPSG:4326'
-                }),
-            });
-            map.setTarget(el[0]);
-        });
-    }
 
     storageLoactions: Array<StorageLoaction> = [
         new StorageLoaction('1234', 'Storage Loaction 1', 'This is the first storage loaction', 6,40.730610, -73.935242),
--- src/views/Material/Material.vue
+++ src/views/Material/Material.vue
@@ -3,7 +3,6 @@
     <h1>Material</h1>
     <v-container>
       <v-card max-width="300" v-for="(storageLocation, i) in storageLoactions" :key="i">
-        <div class="map" :ref="storageLocation.id"></div>
         <v-card-title>{{ storageLocation.name }}</v-card-title>
         <v-card-text>{{ storageLocation.description }}</v-card-text>
         <v-card-subtitle>Total Items: {{ storageLocation.itemCount }}</v-card-subtitle>
