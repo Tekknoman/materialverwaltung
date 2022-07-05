2e6c2ae87827307facdc594be2bd3e004b92c0d0
ElioiMusk <elioteuber@outlook.com>
2022-07-05 23:37:05 +0100

    testing the script

--- remove-unused-builds.sh
+++ remove-unused-builds.sh
@@ -1,17 +1,18 @@
-git branch -r | grep -v '\->' > /tmp/branches.txt
+git branch -r | grep -v '\->' | sed 's|origin/||g' | sed 's| ||g' > /tmp/branches.txt
 echo "Branches:"
 cat /tmp/branches.txt
-find . -type d -print | sed 's/\.\///g' > /tmp/dirs.txt
+find . -type d -print | grep -v 'node_modules' | grep -v '.git' | grep -v 'js' | grep -v 'css' | sed 's/\.\///g' > /tmp/dirs.txt
 echo "Dirs:"
 cat /tmp/dirs.txt
-comm --nocheck-order -23 /tmp/branches.txt /tmp/dirs.txt > /tmp/unused.txt
+comm --nocheck-order -13 /tmp/branches.txt /tmp/dirs.txt > /tmp/unused.txt
 echo "Unused:"
 cat /tmp/unused.txt
 
+
 while read line; do
   echo "Removing $line"
-  rm -rf $line
-done < /tmp/unused.txt
+#  rm -rf $line
+done < /tmp/unused-filtered.txt
 echo "Removing temporary files"
 rm /tmp/branches.txt /tmp/dirs.txt /tmp/unused.txt
-ls -la
+#ls -la
