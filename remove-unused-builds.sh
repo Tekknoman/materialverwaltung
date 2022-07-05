613c2ee9ec6a3353bd02e743ce0646579bfd8a12
ElioiMusk <elioteuber@outlook.com>
2022-07-05 23:47:30 +0100

    should work but no commit

--- remove-unused-builds.sh
+++ remove-unused-builds.sh
@@ -1,18 +1,12 @@
 git branch -r | grep -v '\->' | sed 's|origin/||g' | sed 's| ||g' > /tmp/branches.txt
-echo "Branches:"
-cat /tmp/branches.txt
 find . -type d -print | grep -v 'node_modules' | grep -v '.git' | grep -v 'js' | grep -v 'css' | sed 's/\.\///g' > /tmp/dirs.txt
-echo "Dirs:"
-cat /tmp/dirs.txt
-comm --nocheck-order -13 /tmp/branches.txt /tmp/dirs.txt > /tmp/unused.txt
-echo "Unused:"
-cat /tmp/unused.txt
 
-
-while read line; do
-  echo "Removing $line"
-#  rm -rf $line
-done < /tmp/unused-filtered.txt
+for dir in `cat /tmp/dirs.txt`; do
+  if ! grep -q "$dir" /tmp/branches.txt; then
+    echo "Removing $dir"
+    rm -rf "$dir"
+  fi
+done
 echo "Removing temporary files"
-rm /tmp/branches.txt /tmp/dirs.txt /tmp/unused.txt
-#ls -la
+rm /tmp/branches.txt /tmp/dirs.txt
+ls -la
