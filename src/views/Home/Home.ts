788c74d151c794a723c4014b76876b9ed4cfefff
Elio Teuber <elioteuber@outlook.com>
2022-08-09 17:42:49 +0200

    Added a funny card to the dashboard... looked so empty

--- src/views/Home/Home.ts
+++ src/views/Home/Home.ts
@@ -2,6 +2,27 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 
 @Component({
-  name: 'Home'
+    name: 'Home'
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+    gradients = [
+        ['#222'],
+        ['#42b3f4'],
+        ['red', 'orange', 'yellow'],
+        ['purple', 'violet'],
+        ['#00c6ff', '#F0F', '#FF0'],
+        ['#f72047', '#ffd200', '#1feaea'],
+    ]
+
+    width = 2
+    radius = 10
+    padding = 8
+    lineCap = 'round'
+    gradient = this.gradients[5]
+    value = [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]
+    gradientDirection = 'top'
+    fill = false
+    type = 'trend'
+    autoLineWidth = false
+
+}
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,7 +1,22 @@
 <template>
   <v-main>
     <h1>Dashboard</h1>
-
+    <v-card max-width="300">
+      <v-sparkline
+          :value="value"
+          :gradient="gradient"
+          :smooth="radius || false"
+          :padding="padding"
+          :line-width="width"
+          :stroke-linecap="lineCap"
+          :gradient-direction="gradientDirection"
+          :fill="fill"
+          :type="type"
+          :auto-line-width="autoLineWidth"
+          auto-draw
+      ></v-sparkline>
+      <v-card-text>Lorem ipsum dolor est</v-card-text>
+    </v-card>
   </v-main>
 </template>
 
